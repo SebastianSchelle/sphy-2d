47b8133cd96381eea27ddfb8c9932d198d3bcbee
Sebastian Schelle <sebastianschelle@gmail.com>
2026-04-28 09:04:05 +0200

    minor change

--- scripts/init-sphy-2d.sh
+++ scripts/init-sphy-2d.sh
@@ -9,3 +9,7 @@ echo "Configuring bgfx build..." &&
 cmake .. &&
 echo "Building bgfx..." &&
 make -j16
+
+
+# cmake -S . -B build/release -DCMAKE_BUILD_TYPE=Release
+# cmake --build build/release -j 12 -t run-server
